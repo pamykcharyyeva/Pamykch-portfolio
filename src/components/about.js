9869cea3875bf09d3be1a6cbf76d847c611014e9
Pamyk <pamyk.ch@gmail.com>
2019-08-18 17:45:07 -0400

    done

--- src/components/about.js
+++ src/components/about.js
@@ -8,9 +8,11 @@ class About extends Component {
       <div style={{width: '100%', margin: 'auto'}}>
         <Grid className="about-grid">
           <Cell col={12}>
+            <p></p>
             <img
               src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/68958551_1189987147877352_7006921444693114880_n.jpg?_nc_cat=101&_nc_oc=AQnVTZT8PC21jJVQy8nNSrbk5r9cERaE1OO0Tq0cQZxLA3rhDOA4oCSN2oz2HE7NGnrXftTMF3tqSZ_8WHOYors_&_nc_ht=scontent-lga3-1.xx&oh=5e2e507bd907f8f3fdcbd344325c39b3&oe=5DD8F675"
               alt="avatar"
+              // style={{height: '250px'}}
               className="avatar-img"
               />
             <p></p>
--- src/components/projects.js
+++ src/components/projects.js
@@ -6,8 +6,15 @@ class ProjectContainer extends React.Component {
 		return(
 			<div>
 			<Project 
+				itemClass={"individual-project grey-project"}
+				src={"https://www.youtube.com/embed/9HkgTtW1-Xw"} 
+				title={"My Portfilo"} 
+				p1={"React, ReactMDL, Ant Design "}
+				github={"https://github.com/pamykcharyyeva/Pamykch-portfolio"}
+			/>
+			<Project 
 				itemClass={"individual-project blue-project"}
-				src={"https://www.youtube.com/watch?v=7T0ivP3Emeg&list=PLcFFFaP5zdCc3Dx2LsgPz8XgjQRb85YDX"} 
+				src={"https://www.youtube-nocookie.com/embed/7T0ivP3Emeg"} 
 				title={"Talk & Meet App"} 
 				p1={"Fullstack React/JS and Ruby/Rails Application, Bootstrap "}
 				p2={"Live chatting through Action Cable to access WebSockets"}
@@ -16,7 +23,7 @@ class ProjectContainer extends React.Component {
 			/>
 			<Project 
 				itemClass={"individual-project grey-project"}
-				src={"https://www.youtube.com/watch?v=T4gTNVQcObk&list=PLcFFFaP5zdCc3Dx2LsgPz8XgjQRb85YDX&index=2"} 
+				src={"https://www.youtube-nocookie.com/embed/T4gTNVQcObk"} 
 				title={"Catch the Hearts"} 
 				p1={"Game to Catch as much as possible Hearts"}
 				p2={"Frontend Javasript, HMTL, CSS"}
@@ -25,24 +32,20 @@ class ProjectContainer extends React.Component {
 			/>
 			<Project 
 				itemClass={"individual-project blue-project"}
-				src={"https://www.youtube.com/embed/8LaQP2Ko4C8"} 
-				title={"Postcard"} 
-				p1={"Fullstack Javascript and Ruby/Rails Application"}
-				p2={"Upload photos to a specific location on a map"}
-				p3={"View friends' maps"}
-				github={"https://github.com/carlypecora/Frontend-mod3"}
+				src={"https://www.youtube-nocookie.com/embed/JZSMO-hvtGo"} 
+				title={"Weather App"} 
+				p1={"Built with React, & Open Weather Map API, Bootstrap"}
+				p2={"App to check realtime weather"}
+				github={"https://github.com/pamykcharyyeva/WeatherApp"}
 			/>
-			<Project 
-				itemClass={"individual-project grey-project"}
-				src={"https://www.youtube.com/embed/DAs7lAifncY"} 
-				title={"mRDB"} 
-				p1={"Fullstack Ruby/Rails Application with Auth through BCrypt"}
-				p2={"Login and search from the Spotify API"}
-				p3={"View an artist's albums and rate an artist's songs"}
-			/>
+
 			</div>
 		)
 	}
 }
 
 export default ProjectContainer
+
+
+
+
