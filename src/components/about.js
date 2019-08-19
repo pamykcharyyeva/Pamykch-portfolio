203c0a0ad2739615495d799352c752c24b59e9fb
Pamyk <pamyk.ch@gmail.com>
2019-08-18 23:09:18 -0400

    all done

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ import { Grid, Cell } from 'react-mdl';
 class About extends Component {
   render() {
     return(
-      <div style={{width: '100%', margin: 'auto'}}>
+      <div style={{width: '80%', height: '60%', margin: 'auto'}}>
         <Grid className="about-grid">
           <Cell col={12}>
             <p></p>
